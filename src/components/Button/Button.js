372dc24dd83b050f5d820717bd0973ccbb8ffde0
Wence Cajucom <cajucom.w@gmail.com>
2019-12-31 13:37:27 -0800

    added notes. studied workflow

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,12 +5,14 @@ class Button extends Component {
     render () {
         return (
             <div onClick={this.props.selected} className="Button">
+                {/* "selected" above comes from App.js where "clickCounter"*/}
+                    {/* function is applied */}
                 {this.props.text}
+                {/* "text" above comes from App.js where */}
+                    {/* "Click" text is applied */}
         </div>
         );
     }
 }
 
 export default Button;
-
-      
